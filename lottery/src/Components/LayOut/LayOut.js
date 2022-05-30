3b5f7fb81a04544bafc591d326fea6a7688b80c4
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-05-30 14:16:18 -0400

    add loading effect to the layout

--- lottery/src/Components/LayOut/LayOut.js
+++ lottery/src/Components/LayOut/LayOut.js
@@ -8,22 +8,46 @@ import {
   AppBar,
 } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
+import LoadingButton from "@mui/lab/LoadingButton";
+import SendIcon from "@mui/icons-material/Send";
 import "./LayOut.css";
 const LayOut = (props) => {
   return (
-    <Box sx={{ displayPrint: "flex", direction: "column", bgcolor: "#515151" }}>
-      <AppBar position="static" sx={{ height: "110px" }}>
-        <Toolbar
-          sx={{
-            displayPrint: "flex",
-            justifyContent: "center",
-            height: "110px",
-          }}
-        >
-          <Typography variant="h1">{props.title}</Typography>
-        </Toolbar>
-      </AppBar>
-      {props.children}
+    <Box>
+      <Box
+        sx={{
+          displayPrint: "flex",
+          direction: "column",
+          bgcolor: "#515151",
+          opacity: props.loading ? 0.15 : 1,
+        }}
+      >
+        <AppBar position="static" sx={{ height: "110px" }}>
+          <Toolbar
+            sx={{
+              displayPrint: "flex",
+              justifyContent: "center",
+              height: "110px",
+            }}
+          >
+            <Typography variant="h1">{props.title}</Typography>
+          </Toolbar>
+        </AppBar>
+        {props.children}
+      </Box>
+      {props.loading && (
+        <div className="loading">
+          <LoadingButton
+            endIcon={<SendIcon />}
+            loading={props.loading}
+            loadingPosition="end"
+            variant="contained"
+            size="large"
+          >
+            loading
+          </LoadingButton>
+        </div>
+      )}
     </Box>
   );
 };
