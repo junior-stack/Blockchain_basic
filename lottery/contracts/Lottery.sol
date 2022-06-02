188a6a37a260ea0bda91cc693e9644662863a144
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-06-01 20:00:43 -0400

    Change the contract according to the comments

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
 contract Lottery{
@@ -12,37 +11,39 @@ contract Lottery{
     mapping(uint => address payable) public ticketMapUser;
     uint public price;
     uint public ticket_number;
-    uint public start_time;
+    uint public endTimeThreshold;  // starttimeplusfiveminutes
+    uint public ussage_fee;
     uint public num_sold;
-    ERC20 token;
+    IERC20 token;
     event TransferSent(address _from, address _destAddr, uint _amount);
 
     constructor(address manager1, address manager2, address moktoken) {
-        token = ERC20(moktoken);
+        token = IERC20(moktoken);
         managers.push(manager1);
         managers.push(manager2);
         owner = msg.sender;
-        price = 20;
+        price = 20 * (10 ** 18);
         ticket_number =0;
-        start_time = block.timestamp;
+        endTimeThreshold = block.timestamp + 5 * 60;
         num_sold = 0;
     }
 
     function buyTicket(address from, uint amount) public{
         // 1. transfer token from buyer to this contract account: transfer()
         uint256 quantity = amount * price;
-        token.approve(from, quantity);
-        token.transferFrom(from, address(this), quantity);
-        emit TransferSent(from, address(this), quantity);
-
-        // 2. ticket_number++
-        // 3. assign tickets to buyers
         uint i;
         for(i = 0; i < amount; i++){
             ticketMapUser[ticket_number] = payable(from);
             ticket_number++;
         }
         num_sold += amount;
+        ussage_fee += quantity * 500 / 10000;
+
+        token.transferFrom(from, address(this), quantity);
+
+        // 2. ticket_number++
+        // 3. assign tickets to buyers
+        
 
     }
 
@@ -53,36 +54,41 @@ contract Lottery{
 
     function pickWinner() public{
         //0. check 5 min and whether the game is ongoing: add a field of start time
+        require(endTimeThreshold == block.timestamp, "You need to draw the lottery 5 min after the game");
+        ticket_number = 0;
+
+        num_sold = 0;
+        endTimeThreshold = block.timestamp + 5 * 1000;
 
         // 2. use random to generate winner ticket number
         uint win = random() % ticket_number;
         // 3. Get the winner and send money to him
         uint balance = token.balanceOf(address(this));
-        uint lottery_pool = balance * 95 / 100;  // the owner may not withdraw the accmulative ussage fee of previous lotteries
+        uint lottery_pool = balance - ussage_fee;  // the owner may not withdraw the accmulative ussage fee of previous lotteries
         token.transfer(ticketMapUser[win], lottery_pool);
-        token.transfer(address(owner), balance - lottery_pool);
+        // token.transfer(address(owner), balance - lottery_pool);  remove this line and add withdraw as separate function
         // add the ussage fee
 
         // Reset the game
-        price = 20;
-        ticket_number = 0;
-
-        num_sold = 0;
-        start_time = block.timestamp;
+        
 
     }
 
     function resetPrice(uint newprice) public {
 
         // 2. Check whether the lottery is ongoing
-        require(num_sold == 0, "There has already been some people buying the lottery, u cannot reset the price");
         
         // 3. reset the price
-        price = newprice;
+        price = newprice * (10 ** 18);
     }
 
     function getBalance() public view returns (uint){
         return token.balanceOf(address(this));
+    }
+
+    function withdraw() public {
+        ussage_fee = 0;
+        token.transfer(owner, ussage_fee);
     }
 
 
--- lottery/contracts/MokToken.sol
+++ lottery/contracts/MokToken.sol
@@ -1,18 +1,88 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 
-contract MokToken is ERC20 {
-  constructor(address to) ERC20("MocktailSwap", "MOK"){
-    _mint(to, 10000);
+contract MokToken is IERC20, Context, IERC20Metadata {
+  mapping(address => uint256) private _balances;
+  string private _name;
+  string private _symbol;
+  uint256 private _totalSupply;
+  constructor(address to, uint256 amount){
+    _totalSupply = amount;
+    _name = "MocktailSwap";
+    _symbol = "MOK";
+    _mint(to, amount * (10 ** decimals()));
   }
 
+  function name() public view virtual override returns (string memory) {
+    return _name;
+  }
+
+  function symbol() public view virtual override returns (string memory) {
+    return _symbol;
+  }
+
+  function decimals() public view virtual override returns (uint8) {
+    return 18;
+  }
+
+
   function approve(address spender, uint256 amount) public virtual override returns (bool) {
-        address owner = _msgSender();
-        _approve(spender, owner, amount);
-        return true;
+    return true;
+  }
+
+   function totalSupply() public view virtual override returns (uint256){
+     return _totalSupply;
+   }
+
+   function balanceOf(address account) public view virtual override returns (uint256){
+     return _balances[account];
+   }
+
+   function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual{
+      require(from != address(0), "ERC20: transfer from the zero address");
+      require(to != address(0), "ERC20: transfer to the zero address");
+
+      uint256 fromBalance = _balances[from];
+      unchecked {
+            _balances[from] = fromBalance - amount;
+        }
+        _balances[to] += amount;
     }
+
+   function transfer(address to, uint256 amount) public virtual override returns (bool){
+      address owner = _msgSender();
+      _transfer(owner, to, amount);
+      return true;
+   }
+
+   function allowance(address owner, address spender) public view virtual override returns (uint256){
+     return 0;
+   }
+
+   function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool){
+      _transfer(from, to, amount);
+
+      return true;
+    }
+
+    function _mint(address account, uint256 amount) internal virtual{
+      require(account != address(0), "ERC20: mint to the zero address");
+      _totalSupply += amount;
+      _balances[account] += amount;
+    }
+   
 
 
 }
