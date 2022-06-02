71bfbbf124896c13ecaccd2452b3a11e8bcae1bf
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-06-02 09:05:25 -0400

    add role to contract

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-
-contract Lottery{
-
-    address[] public managers;
-    address public owner;
+contract Lottery is AccessControlEnumerable{
+    bytes32 public constant OWNER = keccak256("owner");
+    bytes32 public constant MANAGER = keccak256("managers");
     mapping(uint => address payable) public ticketMapUser;
     uint public price;
     uint public ticket_number;
@@ -19,9 +18,9 @@ contract Lottery{
 
     constructor(address manager1, address manager2, address moktoken) {
         token = IERC20(moktoken);
-        managers.push(manager1);
-        managers.push(manager2);
-        owner = msg.sender;
+        _setupRole(OWNER, msg.sender);
+        _setupRole(MANAGER, manager1);
+        _setupRole(MANAGER, manager2);
         price = 20 * (10 ** 18);
         ticket_number =0;
         endTimeThreshold = block.timestamp + 5 * 60;
@@ -54,14 +53,16 @@ contract Lottery{
 
     function pickWinner() public{
         //0. check 5 min and whether the game is ongoing: add a field of start time
-        require(endTimeThreshold == block.timestamp, "You need to draw the lottery 5 min after the game");
+        require(endTimeThreshold <= block.timestamp, "You need to draw the lottery 5 min after the game");
+        require(hasRole(OWNER, msg.sender) || hasRole(MANAGER, msg.sender), "Only the managers or owner could pick a winner");
+        uint tick_num = ticket_number;
         ticket_number = 0;
 
         num_sold = 0;
-        endTimeThreshold = block.timestamp + 5 * 1000;
+        endTimeThreshold = block.timestamp + 5 * 60;
 
         // 2. use random to generate winner ticket number
-        uint win = random() % ticket_number;
+        uint win = random() % tick_num;
         // 3. Get the winner and send money to him
         uint balance = token.balanceOf(address(this));
         uint lottery_pool = balance - ussage_fee;  // the owner may not withdraw the accmulative ussage fee of previous lotteries
@@ -77,7 +78,7 @@ contract Lottery{
     function resetPrice(uint newprice) public {
 
         // 2. Check whether the lottery is ongoing
-        
+        require(hasRole(OWNER, msg.sender), "Only the owner could reset the price");
         // 3. reset the price
         price = newprice * (10 ** 18);
     }
@@ -87,11 +88,19 @@ contract Lottery{
     }
 
     function withdraw() public {
+        require(hasRole(OWNER, msg.sender), "You are not the owner of this contract");
+        uint last_ussage = ussage_fee;
         ussage_fee = 0;
-        token.transfer(owner, ussage_fee);
+        token.transfer(getRoleMember(OWNER, 0), last_ussage);
     }
 
+    function owner() public view returns (address){
+        return getRoleMember(OWNER, 0);
+    }
 
+    function managers(uint index) public view returns (address) {
+        return getRoleMember(MANAGER, index);
+    }
 
 }
 
