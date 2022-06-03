ebef73ea28a86febf68e6d94d44d2aa1b852b7fe
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-06-03 15:56:41 -0400

    test recommit

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -1,117 +1,74 @@
 const { assert } = require("chai");
-const {
-  isCallTrace,
-} = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+const { ethers, network } = require("hardhat");
 
-describe("Lottery contract", async () => {
-  let Lottery, lottery, owner, addr1, addr2, Token, token;
-
+describe("pick winner", function(){
+  let  lottery, owner, addr1, addr2, token;
+  let old_balance;
+  this.timeout(10000);
   beforeEach(async () => {
-    Lottery = await ethers.getContractFactory("Lottery");
+    // set up: addr1 becomes the buyer and manager of the lottery, we have addr2 to be the manager and an owner
+    const Lottery = await ethers.getContractFactory("Lottery");
+    const Token = await ethers.getContractFactory("MokToken");
     [owner, addr1, addr2] = await ethers.getSigners();
-    Token = await ethers.getContractFactory("MokToken");
-    token = await Token.deploy();
+    token = await Token.deploy(addr1.address, 500);
+    const old = await token.balanceOf(addr1.address);
+    old_balance = Number(old);
     lottery = await Lottery.deploy(addr1.address, addr2.address, token.address);
-  });
 
-  describe("Deployment", () => {
-    it("should set the right owner: ", async () => {
-      const own = await lottery.owner();
-      assert(own == owner.address);
-    });
-  });
+    //addr1 buys the tickets
+    await lottery.buyTicket(addr1.address, 25);
+  })
 
-  describe("Mok Token", () => {
-    it("Check contract balance", async () => {
-      const amount = 4;
-      const price = await lottery.price();
-      await token.transfer(addr1.address, amount * price);
-      let addr1_balance = await token.balanceOf(addr1.address);
-      const old_contract_balance = await token.balanceOf(lottery.address);
-      assert(Number(addr1_balance) == amount * price);
-      await lottery.buyTicket(addr1.address, amount);
-      addr1_balance = await token.balanceOf(addr1.address);
-      const contract_balance = await token.balanceOf(lottery.address);
-      assert(Number(addr1_balance) == 0);
-      assert(
-        Number(contract_balance) ==
-          Number(old_contract_balance) + amount * price
-      );
-    });
-  });
+  it("checks five minutes", async () => {
+    const signer2 = lottery.provider.getSigner(addr1.address);
+    try{
+      await lottery.connect(signer2).pickWinner();
+      assert(false)
+    } catch(err){
+      assert(true);
+    }
+  })
 
-  describe("Lottery Contract", () => {
-    it("Pick a winner: the receiver can receive money", async () => {
-      const old_owner_balance = await token.balanceOf(owner.address);
-      await token.transfer(addr1.address, old_owner_balance);
-      const new_owner_balance = await token.balanceOf(owner.address);
-      let addr1_balance = await token.balanceOf(addr1.address);
-      assert(Number(new_owner_balance) == 0);
-      assert(Number(addr1_balance) == old_owner_balance);
+  it("check if the one who picks winner is an owner or manager", async () =>{
+    await network.provider.send("evm_increaseTime", [5 * 60]);
+    await network.provider.send("evm_mine");
+    try{
+      const signer = lottery.provider.getSigner("0x246280fD39740213D089f6f3A09745cb52aC5eCA")
+      await lottery.connect(signer).pickWinner();
+      assert(false)
+    } catch(err) {
+      assert(err)
+    }
+   
+    try{
+      const signer2 = lottery.provider.getSigner(addr1.address);
+      await lottery.connect(signer2).pickWinner();
+      assert(true)
+    } catch(err){
+      assert(false);
+    }
+  })
 
-      const amount = 500;
-      const ticket_price = await lottery.price();
-      await lottery.buyTicket(addr1.address, amount);
-      await lottery.pickWinner();
+  it("checks the state is changed correctly: ", async () => {
+    await network.provider.send("evm_increaseTime", [5 * 60]);
+    await network.provider.send("evm_mine");
+    await lottery.pickWinner();
+    const ticket_num = await lottery.ticket_number();
+    const num_sold = await lottery.num_sold();
+    assert(Number(ticket_num) === 0);
+    assert(Number(num_sold) === 0);
+  })
 
-      addr1_balance = await token.balanceOf(addr1.address);
-      const final_ticket_price = await lottery.price();
-      const final_ticket_number = await lottery.ticket_number();
-      const ussage_fee = await lottery.ussage_fee();
-      assert(Number(addr1_balance) == amount * ticket_price * 0.95);
-      assert(Number(ussage_fee) == amount * ticket_price * 0.05);
+  it("checks winner has got right amount", async () => {
+    await network.provider.send("evm_increaseTime", [5 * 60]);
+    await network.provider.send("evm_mine");
+    await lottery.pickWinner();
+    const winner = await lottery.winner();
+    let balance = await token.balanceOf(winner);
+    balance = Number(balance)
+    assert(balance == old_balance * 0.95);
+  })
 
-      // test if reset the game
-      assert(final_ticket_price == 20);
-      assert(final_ticket_number == 0);
-    });
-  });
+})
 
-  describe("Lottery Contract", () => {
-    it("Test Withdraw", async () => {
-      const old_owner_balance = await token.balanceOf(owner.address);
-      await token.transfer(addr1.address, old_owner_balance);
-      const new_owner_balance = await token.balanceOf(owner.address);
-      const addr1_balance = await token.balanceOf(addr1.address);
-      assert(Number(new_owner_balance) == 0);
-      assert(Number(addr1_balance) == Number(old_owner_balance));
 
-      await lottery.buyTicket(addr1.address, 500);
-      await lottery.pickWinner();
-      await lottery.withdraw();
-      const owner_balance = await token.balanceOf(owner.address);
-      const contract_balance = await token.balanceOf(lottery.address);
-      console.log("owner balance: ", Number(owner_balance));
-      console.log("old owner balance: ", Number(old_owner_balance));
-      assert(Number(owner_balance) == Number(old_owner_balance) * 0.05);
-      assert(Number(contract_balance) == 0);
-    });
-  });
-
-  describe("Lottery Contract", () => {
-    it("successful reset price", async () => {
-      // successful reset
-      const Price = 30;
-      await lottery.resetPrice(Price);
-      const newPrice = await lottery.price();
-      assert(newPrice == Price);
-    });
-  });
-
-  describe("Lottery Contract", () => {
-    it("unsuccessful reset price", async () => {
-      // unsuccessful reset
-      try {
-        const Price = 30;
-        const amount = 4;
-        const price = await lottery.price();
-        await token.transfer(addr1.address, amount * price);
-        await lottery.buyTicket(addr1.address, amount);
-        await lottery.resetPrice(Price);
-        assert(false);
-      } catch (err) {
-        assert(err);
-      }
-    });
-  });
-});
